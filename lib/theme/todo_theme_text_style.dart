56074419add43e46fcdb7296d0a989f8501db72a
geonheey <ghyjas0704@gmail.com>
2025-03-05 10:45:18 +0900

    [Feat] TodoThemeTextStyle 생성

---
+++ lib/theme/todo_theme_text_style.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:to_do_list/theme/todo_theme_color.dart';
+
+class TodoThemeTextStyle {
+
+  static const TextStyle darkBold23 = TextStyle(
+    color: TodoThemeColor.dark,
+    fontSize: 23,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle orangeBold23 = TextStyle(
+    color: TodoThemeColor.orange,
+    fontSize: 23,
+    fontWeight: FontWeight.w600,
+  );
+
+}
