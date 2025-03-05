b938cf9b3f08690b22cca2b88b3fc4b04bce9bbb
geonheey <ghyjas0704@gmail.com>
2025-03-05 13:23:41 +0900

    [Feat] 캘린더 커스텀

--- lib/view/component/to_do_calendar.dart
+++ lib/view/component/to_do_calendar.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:table_calendar/table_calendar.dart';
-
+import 'package:to_do_list/theme/todo_theme_color.dart';
 import '../../view_model/task_notifier.dart';
 
 class ToDoCalendar extends ConsumerWidget {
@@ -11,16 +11,62 @@ class ToDoCalendar extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final tasks = ref.watch(taskNotifierProvider).tasks;
+    final selectedDate = ref.watch(taskNotifierProvider).selectedDate;
+
+    print('All tasks: $tasks');
+
     return TableCalendar(
       firstDay: DateTime.utc(2010, 10, 16),
       lastDay: DateTime.utc(2030, 3, 14),
       focusedDay: DateTime.now(),
       selectedDayPredicate: (day) {
-        return isSameDay(day, ref.watch(taskNotifierProvider).selectedDate);
+        return isSameDay(day, selectedDate);
       },
+      headerStyle: HeaderStyle(
+        formatButtonVisible: false,
+        titleCentered: true,
+        leftChevronVisible: false,
+        rightChevronVisible: false,
+      ),
       onDaySelected: (selectedDay, focusedDay) {
         onDateChanged(selectedDay);
       },
+      calendarStyle: CalendarStyle(
+        todayDecoration: BoxDecoration(
+          color: TodoThemeColor.red1,
+          shape: BoxShape.circle,
+        ),
+        selectedDecoration: BoxDecoration(
+          color: TodoThemeColor.orange,
+          shape: BoxShape.circle,
+        ),
+        weekendTextStyle: TextStyle(color: TodoThemeColor.red),
+      ),
+      calendarBuilders: CalendarBuilders(
+        markerBuilder: (context, date, events) {
+          final tasksForDate = tasks.entries
+              .where((entry) => isSameDay(entry.key, date))
+              .map((entry) => entry.value)
+              .expand((taskList) => taskList)
+              .toList();
+
+          print('Tasks for $date: $tasksForDate');
+
+          if (tasksForDate.isNotEmpty) {
+            return Container(
+              width: 8.0,
+              height: 8.0,
+              decoration: const BoxDecoration(
+                color: TodoThemeColor.gray1,
+                shape: BoxShape.circle,
+              ),
+            );
+          }
+
+          return null;
+        },
+      ),
     );
   }
 }
